dbbd6c2a2c2a69627c54ae356e5a6d3ac547c45b
Doyle Brown <dalpiad@gmail.com>
2023-12-18 19:37:29 -0600

    Completed the Unit Testing Studio, but only 7 tests (6 good tests).

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,7 +7,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void noBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Just a string with no brackets."));
+    }
+
+    @Test
+    public void extraBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]"));
+    }
+
+    @Test
+    public void bracketsWithCharsBetweenTrue () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[testing with words between]"));
+    }
+
+    @Test
+    public void balancedBracketsWithinBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[just testing [to be] testing]"));
+    }
+
+    @Test
+    public void bracketInReverseOrderReturnsFalse () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void notDistractedByOtherCharacters() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[{}{{{}]'testing'+ []"));
     }
 }
